60c81a29d38cca30a9761e49f2aa3a8e620ffd28
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-11-16 15:51:43 +0000

    new entities added

--- src/scraper/core/entity.go
+++ src/scraper/core/entity.go
@@ -5,6 +5,13 @@ type SongMeta struct {
 	ArtistLink string `json:"artist_link"`
 	ArtistPage string `json:"artist_page"`
 	ArtistName string `json:"artist_name"`
+	AlbumName  string `json:"album_name"`
+	Date       string `json:"date"`
+	Duration   string `json:"duration"`
+	Start      string `json:"start"`
+	Bitrate    uint8  `json:"bitrate"`
+	Track      string `json:"track"`
+	Genre      string `json:"genre"`
 	Url        string `json:"url"`
 	YtUrl      string `json:"yt_url"`
 	SongID     string `json:"song_id"`
