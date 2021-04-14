bd376841e61638c408efb251910e223d583bf984
L04DB4L4NC3R <angadsharma1016@gmail.com>
2021-04-15 00:08:50 +0530

    ffmpeg workerpool

--- src/ytber/pkg/service.go
+++ src/ytber/pkg/service.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"runtime"
 	"strconv"
 	"strings"
 	"time"
@@ -18,11 +19,12 @@ import (
 )
 
 const (
-	STATUS_META_FED     = "FED"
-	STATUS_DWN_QUEUED   = "QUEUED"
-	STATUS_DWN_FAILED   = "FAILED"
-	STATUS_DWN_COMPLETE = "COMPLETED" // song downloaded (before thumbnail application)
-	STATUS_FINISHED     = "FINISHED"  // thumbnail applied
+	STATUS_META_FED         = "FED"
+	STATUS_DWN_QUEUED       = "QUEUED"
+	STATUS_DWN_FAILED       = "FAILED"
+	STATUS_DWN_COMPLETE     = "COMPLETED" // song downloaded (before thumbnail application)
+	STATUS_THUMBNAIL_FAILED = "THUMBNAIL_FAILED"
+	STATUS_FINISHED         = "FINISHED" // thumbnail applied
 
 	YT_BASE_URL = "https://youtube.com/watch?v="
 
@@ -63,6 +65,11 @@ const (
 	MAX_RETRIES = 3
 )
 
+var (
+	// maximum ffmpeg processes
+	MAXPROCS = runtime.NumCPU()
+)
+
 type service struct {
 	redis Repository
 	cerr  chan AsyncErrors
@@ -78,8 +85,8 @@ type Service interface {
 	SongDownload(ctx context.Context, req *pb.SongMetaRequest) (*pb.SongMetaResponse, error)
 	PlaylistDownload(ctx context.Context, req *pb.PlaylistMetaRequest) (*pb.PlaylistMetaResponse, error)
 	offloadToYoutubeDL(ctx context.Context, format string, query string, songmeta *pb.SongMetaRequest) (postprocessingcmd asyncReturn)
-	offloadBatchToYoutubeDL(ctx context.Context, slice []*pb.SongMetaRequest, fetchChan chan asyncReturn) (postprocessingcmds []asyncReturn, retries []*pb.SongMetaRequest)
-	applyThumbnailsSerially(ctx context.Context, thumbscmds []string)
+	offloadBatchToYoutubeDL(ctx context.Context, slice []*pb.SongMetaRequest, fetchChan chan asyncReturn) (postprocessingcmds []asyncReturn, retries []*pb.SongMetaRequest, resources []*pb.SongMetaRequest)
+	ApplyThumbnails(ctx context.Context, thumbscmds []string, resources []*pb.SongMetaRequest)
 }
 
 func (s *service) SongDownload(ctx context.Context, req *pb.SongMetaRequest) (*pb.SongMetaResponse, error) {
@@ -92,7 +99,7 @@ func (s *service) SongDownload(ctx context.Context, req *pb.SongMetaRequest) (*p
 
 	go func() {
 		thumbscmd := s.offloadToYoutubeDL(ctx, "mp3", query, req)
-		s.applyThumbnailsSerially(ctx, []string{thumbscmd.postprocessingcmd})
+		s.ApplyThumbnails(ctx, []string{thumbscmd.postprocessingcmd}, []*pb.SongMetaRequest{req})
 		s.redis.UpdateStatus(RESOURCE_SONG, req.SongId, STATUS_FINISHED)
 	}()
 
@@ -110,11 +117,15 @@ func (s *service) PlaylistDownload(ctx context.Context, req *pb.PlaylistMetaRequ
 
 	go func(count int) {
 		PLAYLIST_BATCH_SIZE, _ := strconv.Atoi(os.Getenv("PLAYLIST_BATCH_SIZE"))
+		if PLAYLIST_BATCH_SIZE == 0 {
+			PLAYLIST_BATCH_SIZE = MAXPROCS
+		}
 		var (
-			batchCount   int     = 1
-			retryCount   int     = 0
-			totalBatches float64 = math.Ceil(float64(count) / float64(PLAYLIST_BATCH_SIZE))
-			retries      []*pb.SongMetaRequest
+			batchCount         int     = 1
+			retryCount         int     = 0
+			totalBatches       float64 = math.Ceil(float64(count) / float64(PLAYLIST_BATCH_SIZE))
+			retries, resources []*pb.SongMetaRequest
+			thumbscmds         []string
 		)
 
 		fetchChan := make(chan asyncReturn, PLAYLIST_BATCH_SIZE)
@@ -134,21 +145,20 @@ func (s *service) PlaylistDownload(ctx context.Context, req *pb.PlaylistMetaRequ
 				"batch_index":       i,
 				"batch_offset":      offset,
 			}).Info("playlist batch execution")
-			results, retry := s.offloadBatchToYoutubeDL(ctx, req.Songs[i:offset], fetchChan)
-			go s.applyThumbnailsSerially(ctx, results)
+			results, retry, metas := s.offloadBatchToYoutubeDL(ctx, req.Songs[i:offset], fetchChan)
+			thumbscmds = append(thumbscmds, results...)
+			resources = append(resources, metas...)
 			retries = append(retries, retry...)
 			batchCount++
 		}
 
+		s.ApplyThumbnails(ctx, thumbscmds, resources)
 		log.WithFields(log.Fields{
 			"total_songs_count": count,
 			"total_batches":     totalBatches,
 			"batch_size":        PLAYLIST_BATCH_SIZE,
 			"time_taken":        time.Since(dispatchTime).Minutes(),
 		}).Info("Download Successful")
-		// s.redis.UpdateStatus(req.Type, req.ResourceId, STATUS_DWN_COMPLETE)
-		s.redis.UpdateStatus(req.Type, req.ResourceId, STATUS_FINISHED)
-
 		// using a closure for retry
 		func() {
 			numRetries := len(retries)
@@ -171,6 +181,7 @@ func (s *service) PlaylistDownload(ctx context.Context, req *pb.PlaylistMetaRequ
 				)
 			}
 		}()
+		s.redis.UpdateStatus(req.Type, req.ResourceId, STATUS_FINISHED)
 	}(count)
 
 	res := &pb.PlaylistMetaResponse{
@@ -200,7 +211,7 @@ func (s *service) offloadToYoutubeDL(
 	metacommand := fmt.Sprintf(YT_DOWNLOAD_METADATA_ARGS, artistName, songTitle, songmeta.Date, songmeta.Url, string(songmeta.Track))
 
 	downloadcommand := command + metacommand + YT_DOWNLOAD_PATH_CMD
-	go s.redis.UpdateStatus(RESOURCE_SONG, songmeta.SongId, STATUS_DWN_QUEUED)
+	s.redis.UpdateStatus(RESOURCE_SONG, songmeta.SongId, STATUS_DWN_QUEUED)
 
 	cmd := exec.CommandContext(ctx, "sh", "-c", downloadcommand)
 	out, err := cmd.Output()
@@ -209,7 +220,7 @@ func (s *service) offloadToYoutubeDL(
 		log.WithFields(log.Fields{
 			"error": err.Error(),
 		}).Info("error executing download")
-		go s.redis.UpdateStatus(RESOURCE_SONG, songmeta.SongId, STATUS_DWN_FAILED)
+		s.redis.UpdateStatus(RESOURCE_SONG, songmeta.SongId, STATUS_DWN_FAILED)
 		return asyncReturn{
 			query:             query,
 			meta:              songmeta,
@@ -217,7 +228,7 @@ func (s *service) offloadToYoutubeDL(
 		}
 	}
 
-	go s.redis.UpdateStatus("song", songmeta.SongId, STATUS_DWN_COMPLETE)
+	s.redis.UpdateStatus(RESOURCE_SONG, songmeta.SongId, STATUS_DWN_COMPLETE)
 	log.WithFields(log.Fields{
 		"song": query,
 	}).Info("download completed")
@@ -259,7 +270,11 @@ func (s *service) offloadToYoutubeDL(
 	}
 }
 
-func (s *service) offloadBatchToYoutubeDL(ctx context.Context, slice []*pb.SongMetaRequest, fetchChan chan asyncReturn) (postprocessingcmds []string, retry []*pb.SongMetaRequest) {
+func (s *service) offloadBatchToYoutubeDL(ctx context.Context, slice []*pb.SongMetaRequest, fetchChan chan asyncReturn) (
+	postprocessingcmds []string,
+	retry []*pb.SongMetaRequest,
+	resources []*pb.SongMetaRequest,
+) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), BATCH_DOWNLOAD_TIMEOUT)
 	defer cancel()
@@ -280,13 +295,14 @@ func (s *service) offloadBatchToYoutubeDL(ctx context.Context, slice []*pb.SongM
 	for {
 		select {
 		case <-ctx.Done():
-			return postprocessingcmds, retry
+			return postprocessingcmds, retry, resources
 		case result := <-fetchChan:
 			if count == batchSize-1 {
-				return postprocessingcmds, retry
+				return postprocessingcmds, retry, resources
 			}
 			if result.postprocessingcmd != "" {
 				postprocessingcmds = append(postprocessingcmds, result.postprocessingcmd)
+				resources = append(resources, result.meta)
 			} else {
 				retry = append(retry, result.meta)
 			}
@@ -299,22 +315,76 @@ func (s *service) offloadBatchToYoutubeDL(ctx context.Context, slice []*pb.SongM
 
 // ffmpeg encoding with thumbnail is best done serially
 // after all the songs are downloaded
-func (s *service) applyThumbnailsSerially(ctx context.Context, thumbscmds []string) {
+func (s *service) ApplyThumbnails(ctx context.Context, thumbscmds []string, resources []*pb.SongMetaRequest) {
+	var (
+		nctx, cancel = context.WithTimeout(context.Background(), THUMBNAIL_APPLICATION_TIMEOUT)
+		n            = len(thumbscmds)
+		errChan      = make(chan *CmdError, n)
+		cmdChan      = make(chan *exec.Cmd, n)
+		failedCmds   []*exec.Cmd
+	)
 
-	log.Infof("Queuing %d thumbnail jobs", len(thumbscmds))
-	command := strings.Join(thumbscmds, ";")
+	defer cancel()
+	log.Infof("queuing %d thumbnail jobs", n)
 
-	ctx, _ = context.WithTimeout(context.Background(), THUMBNAIL_APPLICATION_TIMEOUT)
-	cmd := exec.CommandContext(ctx, "sh", "-c", command)
+	// initialize worker pool for running commands
+	s.InitExecPool(nctx, n, cmdChan, errChan)
 
-	if err := cmd.Run(); err != nil {
-		log.WithFields(log.Fields{
-			"error": err.Error(),
-		}).Info("error executing thumbnail application")
-		return
+	for _, command := range thumbscmds {
+		cmdChan <- exec.CommandContext(ctx, "sh", "-c", command)
 	}
 
-	log.Info("Thumbnails Applied")
+	for _, v := range resources {
+		cmdErr := <-errChan
+		if cmdErr != nil {
+			log.WithFields(log.Fields{
+				"error": cmdErr.Err.Error(),
+			}).Info("error executing thumbnail application")
+			failedCmds = append(failedCmds, cmdErr.Cmd)
+			s.redis.UpdateStatus(RESOURCE_SONG, v.SongId, STATUS_THUMBNAIL_FAILED)
+		} else {
+			s.redis.UpdateStatus(RESOURCE_SONG, v.SongId, STATUS_FINISHED)
+		}
+	}
+	log.Info("%d thumbnails applied, %d failed", n, len(failedCmds))
+}
+
+type CmdError struct {
+	Err error
+	Cmd *exec.Cmd
+}
+
+func (s *service) InitExecPool(
+	ctx context.Context,
+	count int,
+	cmdChan chan *exec.Cmd, // context bound cmds
+	errChan chan *CmdError,
+) {
+	if count > MAXPROCS {
+		count = MAXPROCS
+	}
+	for i := 0; i < count; i++ {
+		go func() {
+			for cmd := range cmdChan {
+				if err := cmd.Run(); err != nil {
+					errChan <- &CmdError{
+						Err: err,
+						Cmd: cmd,
+					}
+				}
+				errChan <- nil
+			}
+		}()
+	}
+	// wait for context to signal teardown
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			time.Sleep(time.Second * time.Duration(1))
+		}
+	}
 }
 
 func Register(rdc Repository, cerr chan AsyncErrors) error {
